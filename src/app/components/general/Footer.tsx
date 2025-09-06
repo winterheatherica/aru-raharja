10f824929942b62d37d98cdf0c428f9f0f8db297
zakirio-hw <zakirio.hugoraazaq.wasis.tik22@mhsw.pnj.ac.id>
2025-09-06 19:04:20 +0700

    Footer Fix

--- src/app/components/general/Footer.tsx
+++ src/app/components/general/Footer.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export function Footer() {
     return (
-        <footer className="footer p-10 flex-col items-center bottom-0 bg-gray-300">
-            <div className="flex justify-center items-center w-full mb-8">
+        <footer className="footer p-10 flex-col items-center bottom-0 bg-gray-300 w-full">
+            <div className="flex justify-center w-full mb-8">
                 <Image
                     src={"/footer/Desc.svg"}
                     alt={"Desc"}
@@ -11,19 +11,23 @@ export function Footer() {
                     height={0}
                 />
             </div>
-            <div className="flex justify-center items-center gap-40 w-full">
-                <Image
-                    src={"/footer/Address.svg"}
-                    alt={"Desc"}
-                    width={240}
-                    height={0}
-                />
-                <Image
-                    src={"/footer/ContactUs.svg"}
-                    alt={"Desc"}
-                    width={150}
-                    height={0}
-                />
+            <div className="flex w-full items-center justify-between">
+                <div className="flex w-full items-center justify-center">
+                    <Image
+                        src={"/footer/Address.svg"}
+                        alt={"Desc"}
+                        width={240}
+                        height={0}
+                    />
+                </div>
+                <div className="flex w-full items-center justify-center">
+                    <Image
+                        src={"/footer/ContactUs.svg"}
+                        alt={"Desc"}
+                        width={150}
+                        height={0}
+                    />
+                </div>
             </div>
         </footer>
     )
