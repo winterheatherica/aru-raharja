baceb9bb14cfc849745b020dcab2eacad09f762e
winterheatherica <winterheatherica@gmail.com>
2025-09-08 21:10:22 +0700

    ketinggalan wkwk

--- src/components/section/HomePage/StatsCtaMap/CtaButton.tsx
+++ src/components/section/HomePage/StatsCtaMap/CtaButton.tsx
@@ -16,7 +16,7 @@ export default function CtaButton({ href, label }: { href: string; label: string
       "
     >
       <div className="flex items-center gap-3">
-        <span className="text-xl font-normal">{label}</span>
+        <span className="text-xl md:text-2xl font-bold">{label}</span>
         <svg width="40" height="40" viewBox="0 0 24 27" fill="none" xmlns="http://www.w3.org/2000/svg" className="w-2 h-2">
           <path
             d="M23 11.2H20.1111C19.0065 11.2 18.1111 12.0954 18.1111 13.2V16C18.1111 17.1046 19.0065 18 20.1111 18H23M23 11.2V18M23 11.2C23 5.5667 18.0751 1 12 1C5.92487 1 1 5.5667 1 11.2M23 18V18C23 22.1421 19.6421 25.5 15.5 25.5H15M1 11.2V16C1 17.1046 1.89543 18 3 18H3.88889C4.99346 18 5.88889 17.1046 5.88889 16V13.2C5.88889 12.0954 4.99346 11.2 3.88889 11.2H1ZM15 25.5V25.5C15 24.1193 13.8807 23 12.5 23H10.25C9.55964 23 9 23.5596 9 24.25V24.25C9 24.9404 9.55964 25.5 10.25 25.5H15Z"
